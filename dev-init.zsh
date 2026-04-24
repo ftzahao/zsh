ca96d3014f9c556a356c2da5278a30f90d0932c5
ftzahao <ftzahao@icloud.com>
2026-04-24 16:25:50 +0800

    添加 dev-init.zsh 和 plugin.zsh 文件
    在 dev-init.zsh 中添加 mise 和 starship 的初始化配置。
    在 plugin.zsh 中添加 zsh-autosuggestions、fast-syntax-highlighting 和 history-search-multi-word 插件的加载配置。

---
+++ dev-init.zsh
@@ -0,0 +1,2 @@
+[ -x "$(command -v mise)" ] && eval "$(mise activate zsh)"
+[ -x "$(command -v starship)" ] && eval "$(starship init zsh)"
---
+++ plugin.zsh
@@ -0,0 +1,8 @@
+zinit ice wait lucid
+zinit light zsh-users/zsh-autosuggestions
+
+zinit ice wait lucid
+zinit light zdharma-continuum/fast-syntax-highlighting
+
+zinit ice wait lucid
+zinit light zdharma-continuum/history-search-multi-word
